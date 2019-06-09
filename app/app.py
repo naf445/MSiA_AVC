ec631c2a8fadaaec4cf194f100734956fc1b16e2
Nathan Franklin <nathanfranklin2019@u.northwestern.edu>
2019-06-09 16:30:42 -0500

    Work on web server application and returning results
    This commit made major changes to the app.py file which runns my flask app. It has 2 routes, a home page
    and a results page where results are displayed.
    
    I created a main .html template and inherited the structure to my others.
    
    I allowed for a form to be on the home page where a new plot summary can be entered and results will be gotten from that.

--- app/app.py
+++ app/app.py
@@ -5,6 +5,10 @@ import yaml
 import pandas as pd
 import logging
 import logging.config
+import gensim.downloader as api
+import sys; sys.path.append('../src/helpers')
+from scoring import score_user_input
+from flask import url_for
 
 directory_abs_path = str(os.path.dirname(os.path.abspath(__file__)))
 
@@ -15,16 +19,30 @@ with open(directory_abs_path+"/../config/flask_config.yml", "r") as yml_file:
 logging.config.fileConfig(directory_abs_path+config["logger_config"], disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-logger.info("Load in RFC .pkl")
-# Use pickle to load in the pre-trained model.
-with open(directory_abs_path+'/../models/RFC_deployment_model.pkl', 'rb') as f:
-    rfc = pickle.load(f)
+logger.info("Loading in word2vec model")
+word2vec_model = api.load("glove-wiki-gigaword-50")
+
+logger.info("Loading in lda model")
+with open(directory_abs_path+config['infile_lda'], 'rb') as file:  
+    lda_model = pickle.load(file)
+    
+logger.info("Loading in tf_vect model")
+with open(directory_abs_path+config['infile_tf_vect'], 'rb') as file:  
+    tf_vect = pickle.load(file)
+
+logger.info("Loading in RFC model")
+with open(directory_abs_path+config['infile_model'], 'rb') as file:  
+    rfc = pickle.load(file)
+
+
+
 
 logger.info("Instantiate flask app object")
 app = flask.Flask(__name__, template_folder='templates')
 
 logger.info("create main route")
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def main():
 
     if flask.request.method == 'GET':
@@ -32,21 +50,34 @@ def main():
     	return(flask.render_template('main.html'))
 
     if flask.request.method == 'POST':
-    	logger.info("POST request received by host server")
-    	author = flask.request.form['author']
-    	plot_summary = flask.request.form['plot summary']
+        
+        logger.info("POST request received by host server")
+        author = flask.request.form['author']
+        plot_summary = flask.request.form['plot summary']
+        model_results = score_user_input.score_model(plot_summary, word2vec_model, tf_vect, lda_model, rfc)
 
-    	return flask.render_template('main.html',
-    						original_input={'author':author,
-    										'plot summary':plot_summary})
+        return flask.render_template('main.html')
 
 
+@app.route("/results", methods=['GET', 'POST'])
+def results():
 
-logger.info("Perform __name__==__main__ check")
+    if flask.request.method == 'POST':
+        
+        logger.info("POST request received by host server")
+        author = flask.request.form['author']
+        plot_summary = flask.request.form['plot_summary']
+        model_results = score_user_input.score_model(plot_summary, word2vec_model, tf_vect, lda_model, rfc)
+
+        return flask.render_template('results.html',\
+                        original_input = {'author': author,'plot_summary': plot_summary}, \
+                        model_results = model_results)
+
+logger.info("Perform __name__ == __main__ check")
 if __name__ == '__main__':
 	
-	logger.info("Run App")
-	app.run(debug=True)
+    logger.info("Run App")
+    app.run(debug=False)
 
 
 
