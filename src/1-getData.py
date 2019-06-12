fbd85c4c6ef894b392d768fac5c9535e7fef984a
Nathan Franklin <nathanfranklin2019@u.northwestern.edu>
2019-06-12 07:35:06 -0500

    add s3 functionality for getting data

---
+++ src/1-getData.py
@@ -0,0 +1,53 @@
+import urllib.request 
+import tarfile
+import os
+import boto3
+import sys; sys.path.append('helpers')
+import logging
+import logging.config
+import yaml
+import shutil
+
+directory_abs_path = str(os.path.dirname(os.path.abspath(__file__)))
+
+# import config yaml file
+with open(directory_abs_path+"/../config/src_config.yml", "r") as yml_file:
+    src_config = yaml.load(yml_file)
+src_config = src_config['src']
+
+# import config yaml file
+with open(directory_abs_path+"/../config/src_config.yml", "r") as yml_file:
+    config = yaml.load(yml_file)
+config = config['get_data']
+
+logging.config.fileConfig(directory_abs_path+config["logger_config"], disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+logger.info("download zipped data to local file system")
+urllib.request.urlretrieve(config['download_url'],\
+	directory_abs_path + config['tar_file'])
+
+logger.info("extract contents from zipped data and delete zipped version")
+tf = tarfile.open(directory_abs_path + config['tar_file'])
+logger.info("save to {}".format(directory_abs_path + config['extraction_point']))
+tf.extractall(path=directory_abs_path + config['extraction_point'])
+tf.close()
+os.remove(directory_abs_path + config['tar_file'])
+
+
+if src_config['s3']['use']:
+	logger.info("s3 option initiated")
+	#s3 = boto3.resource("s3")
+	s3 = boto3.client('s3')
+	logger.info("connect to S3 & upload file to bucket")
+	s3.upload_file(Bucket=src_config['s3']['bucket_name'],
+	               Filename=directory_abs_path + config['outfile'], 
+	               Key=src_config['s3']['booksummaries.txt'])
+	logger.info("remove local data")
+	shutil.rmtree(directory_abs_path + config['outdirectory'])
+
+
+
+
+
+
--- src/1-getData.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bucket=$1
-curl -sL http://www.cs.cmu.edu/~dbamman/data/booksummaries.tar.gz | tar xz
-mv booksummaries/ data/
-aws s3 cp --recursive data/booksummaries $bucket
